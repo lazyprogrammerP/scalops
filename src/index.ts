9ede0d9755b36d91d32744616835dbb05cf7581d
DanglingDynamo <vedant.matanhelia@gmail.com>
2024-04-13 04:50:13 +0530

    feat: postgresql

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import * as dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import clerkAuth from './middleware/clerkAuth';
+import * as dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import clerkAuth from "./middleware/clerkAuth";
+import prisma from "../prisma";
 
 dotenv.config();
+
+console.log(process.env.DATABASE_URL);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ message: 'Hello', status: 'success' });
+prisma.$connect().then(() => console.log("connected"));
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello", status: "success" });
 });
 
-app.get('/test', clerkAuth, (req: Request, res: Response) => {
-    res.status(200).json({ message: 'you are authenticated', status: 'success' });
+app.get("/test", clerkAuth, (req: Request, res: Response) => {
+    res.status(200).json({ message: "you are authenticated", status: "success" });
 });
 
 app.listen(port, () => {
