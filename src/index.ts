6ac3eff15330ef500a04212982b821961e9f5423
Parshva Runwal <runwalparshva3@gmail.com>
2024-04-13 02:54:29 +0530

    Prisma set-up and decluttered the project

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import * as dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+import * as dotenv from "dotenv";
+import express, { Request, Response } from "express";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
 });
 
 app.listen(port, () => {
-    console.log(`server running on http://localhost:${port}`);
+  console.log(`server running on http://localhost:${port}`);
 });
