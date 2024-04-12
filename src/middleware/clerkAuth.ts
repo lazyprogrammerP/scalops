6c77b9c7cabbc3ba04f3fab71009a2c22cd57d06
Parshva Runwal <runwalparshva3@gmail.com>
2024-04-13 03:35:01 +0530

    Merged clerkAuth updates

--- src/middleware/clerkAuth.ts
+++ src/middleware/clerkAuth.ts
@@ -1,34 +1,28 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export default function clerkAuth(
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) {
-    const publicKey = process.env.CLERK_JWT_PEM_KEY || "";
-    const token = req.headers.authorization;
+export default function clerkAuth(req: Request, res: Response, next: NextFunction) {
+  const publicKey = process.env.CLERK_JWT_PEM_KEY || "";
+  const token = req.headers.authorization;
 
-    if (token === undefined) {
-        res
-            .status(401)
-            .json({ message: "user not signed in", status: "fail", data: {} });
+  if (token === undefined) {
+    res.status(401).json({ message: "user not signed in", status: "fail", data: {} });
 
-        return;
-    }
+    return;
+  }
 
-    if (publicKey === "") {
-        console.log("clerk jwt pem key not found");
-        process.exit(1);
-    }
+  if (publicKey === "") {
+    console.log("clerk jwt pem key not found");
+    process.exit(1);
+  }
 
-    try {
-        let session = token.split(" ")[1];
-        let decoded = jwt.verify(session, publicKey);
-        console.log(decoded);
+  try {
+    let session = token.split(" ")[1];
+    let decoded = jwt.verify(session, publicKey);
+    console.log(decoded);
 
-        next();
-    } catch (error) {
-        res.status(400).json({ message: "invalid token", status: "fail" });
-    }
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "invalid token", status: "fail" });
+  }
 }
